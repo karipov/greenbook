9e604c895daab03b6e5aa169705bf2107ed72798
RO <ro@RO.local>
2022-10-15 12:28:45 -0400

    test

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>ugh</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {6*6}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.imdb.com/title/tt6966692/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GreenBook
         </a>
       </header>
     </div>
